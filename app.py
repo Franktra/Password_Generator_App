83a1c66af2ca23c85e3ac9b2658803a22e1aa72c
Traci F <132582971+Franktra@users.noreply.github.com>
2023-07-02 13:38:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-
-import streamlit as st
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import streamlit as st
 
-chatbot = ChatBot('GuardianAngels')
+# Create a ChatBot instance
+chatbot = ChatBot('MyChatBot')
+
+# Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("chatterbot.corpus.english")
 
-@st.cache(allow_output_mutation=True)
-def get_chatbot():
-    return ChatBot('GuardianAngels')
+# Train the chatbot based on the English corpus
+trainer.train('chatterbot.corpus.english')
 
 def get_response(user_input):
-    chatbot = get_chatbot()
+    # Get a response from the chatbot based on user input
     response = chatbot.get_response(user_input)
     return str(response)
 
 def main():
-    st.title('Chatbot Demo')
-    user_input = st.text_input('User Input')
+    st.title("ChatterBot")
+    st.write("Welcome! Start chatting with the chatbot.")
 
-    if st.button('Submit'):
-        response = get_response(user_input)
-        st.text_area('Bot Response', value=response)
+    user_input = st.text_input("User Input:")
+    submit_button = st.button("Submit")
 
-if __name__ == '__main__':
+    if submit_button:
+        bot_response = get_response(user_input)
+        st.write("Bot Response:", bot_response)
+
+if __name__ == "__main__":
     main()
