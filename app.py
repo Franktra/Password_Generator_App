abdbcef79fdce69bd840639c6a89773c427a5d72
Traci F <132582971+Franktra@users.noreply.github.com>
2023-07-02 13:33:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import streamlit as st
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
-def chatbot_response(user_input):
-    # Define your chatbot logic here
-    if user_input.lower() == "hello":
-        return "Hi! How can I assist you?"
-    elif user_input.lower() == "goodbye":
-        return "Goodbye! Have a great day!"
-    else:
-        return "I'm sorry, I don't understand. Can you please rephrase?"
+chatbot = ChatBot('GuardianAngels')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+
+@st.cache(allow_output_mutation=True)
+def get_chatbot():
+    return ChatBot('GuardianAngels')
+
+def get_response(user_input):
+    chatbot = get_chatbot()
+    response = chatbot.get_response(user_input)
+    return str(response)
 
 def main():
-    st.title("Chatbot")
-    st.write("Welcome! Enter your message below.")
+    st.title('Chatbot Demo')
+    user_input = st.text_input('User Input')
 
-    user_input = st.text_input("User Input:")
-    submit_button = st.button("Submit")
+    if st.button('Submit'):
+        response = get_response(user_input)
+        st.text_area('Bot Response', value=response)
 
-    if submit_button:
-        bot_response = chatbot_response(user_input)
-        st.write("Bot Response:", bot_response)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
